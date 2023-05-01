d877073e841b4784061ae1a8b8c8ff30bee3dd58
nicolasCatalanoSoutherncode <nicolas.catalano@southerncode.us>
2023-05-01 18:42:13 -0300

    fix: delete initial controller.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductsController } from './controllers/products.controller';
-import { CategoriesController } from './controllers/categories.controller';
-import { BrandsController } from './controllers/brands.controller';
-import { UsersController } from './controllers/users.controller';
-import { OrdersController } from './controllers/orders.controller';
-import { CustomersController } from './controllers/customers.controller';
-import { ProductsService } from './services/products.service';
-import { CategoriesService } from './services/categories.service';
-import { BrandsService } from './services/brands.service';
-import { CustomersService } from './services/customers.service';
+import { Module } from "@nestjs/common";
+import { ProductsController } from "./controllers/products.controller";
+import { CategoriesController } from "./controllers/categories.controller";
+import { BrandsController } from "./controllers/brands.controller";
+import { UsersController } from "./controllers/users.controller";
+import { OrdersController } from "./controllers/orders.controller";
+import { CustomersController } from "./controllers/customers.controller";
+import { ProductsService } from "./services/products.service";
+import { CategoriesService } from "./services/categories.service";
+import { BrandsService } from "./services/brands.service";
+import { CustomersService } from "./services/customers.service";
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductsController, CategoriesController, BrandsController, UsersController, OrdersController, CustomersController],
-  providers: [AppService, ProductsService, CategoriesService, BrandsService, CustomersService],
+  controllers: [ProductsController, CategoriesController, BrandsController, UsersController, OrdersController, CustomersController],
+  providers: [ProductsService, CategoriesService, BrandsService, CustomersService]
 })
-export class AppModule {}
+export class AppModule {
+}
