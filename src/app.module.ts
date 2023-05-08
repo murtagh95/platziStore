d1f35d9ecb14a593dec99dd9bdc6349677e91399
Murtagh95 <nec.catalano@gmail.com>
2023-05-08 01:00:54 -0300

    fix: environment variable validations added.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,17 @@
 import { Module } from '@nestjs/common';
 import { HttpModule, HttpService } from '@nestjs/axios';
 import { lastValueFrom } from 'rxjs';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import * as process from 'process';
+
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { ConfigModule } from '@nestjs/config';
 import { environments } from '../environments';
-import * as process from 'process';
+import config from '../config';
 
 @Module({
   controllers: [AppController],
@@ -19,7 +22,13 @@ import * as process from 'process';
     DatabaseModule,
     ConfigModule.forRoot({
       envFilePath: environments[process.env.NODE_ENV] || '.env',
+      load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATABASE_NAME: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+      }),
     }),
   ],
   providers: [
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,15 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigType } from '@nestjs/config';
+import config from '../config';
 
 @Injectable()
 export class AppService {
   constructor(
-    private configService: ConfigService,
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
     @Inject('TASKS') private tasks: any[],
   ) {}
   getHello(): string {
-    const api_key = this.configService.get<string>('API_KEY');
-    return `Hello World! With api_key => ${api_key}`;
+    return `Hello World! With api_key => ${this.configService.apiKey} and database => ${this.configService.database.name}`;
   }
 
   geTasks() {
