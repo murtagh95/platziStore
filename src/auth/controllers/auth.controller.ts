14de7248c560607d0ab60534c8db50b72cf6e5d9
NicolasCatalanoModo <nicolas.catalano@modo.com.ar>
2023-05-14 00:10:56 -0300

    feat: jwt is added at login.

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -2,12 +2,19 @@ import { Controller, Req, Post, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
 import { ApiTags } from '@nestjs/swagger';
+
+import { AuthService } from '../services/auth.service';
+import { User } from '../../users/entities/users.entity';
+
 @ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @UseGuards(AuthGuard('local'))
   @Post('login')
   login(@Req() req: Request) {
-    return req.user;
+    const user = req.user as User;
+    return this.authService.generateJWT(user);
   }
 }
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -2,17 +2,34 @@ import { Injectable } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 
 import { UsersService } from '../../users/services/users.service';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../../users/entities/users.entity';
+import { PayloadToken } from '../models/token.model';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UsersService) {}
+  constructor(
+    private userService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(email: string, password: string) {
     const user = await this.userService.findByEmail(email);
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (user && isMatch) {
-      return user;
+    if (user) {
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (isMatch) {
+        return user;
+      }
     }
     return null;
+  }
+
+  generateJWT(user: User) {
+    const payload: PayloadToken = { role: user.role, sub: user._id };
+    const { password: password, ...userPayload } = user.toJSON();
+    return {
+      access_token: this.jwtService.sign(payload),
+      userPayload,
+    };
   }
 }
